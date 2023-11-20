8d772ce93fe7c6a9883ba4bc8b8bc5ce84f49440
Cooper Luce <16cluce@gmail.com>
2023-11-19 19:14:57 -0500

    adding some more solutions

--- chapter-1/section-1/ex-1.7.scm
+++ chapter-1/section-1/ex-1.7.scm
@@ -10,3 +10,40 @@
 ;; Design a square-root procedure that uses this kind of end test.
 ;; Does this work better for small and large numbers?
 
+(define (sqrt-iter guess x)
+  (if (good-enough? guess x)
+      guess
+      (sqrt-iter (improve guess x)
+                 x)))
+
+(define (improve guess x)
+  (average guess (/ x guess)))
+
+(define (average x y)
+  (/ (+ x y) 2))
+
+(define (square x)
+  (* x x))
+
+(define (good-enough? guess x)
+  (< (abs (- (square guess) x)) 0.001))
+
+(define (sicp/sqrt x)
+  (sqrt-iter 1.0 x))
+
+;; for large numbers, this implementation of sqrt enters an infinite
+;; recursion. this is because the relative distance between two large
+;; numbers (for n greater than some N) will never be less than 0.001
+;; -- as the floating point arithmetic simply can't handle the
+;; precision. for small numbers, the answer is trivial: this test is
+;; unable to calculate roots when the true root is << 0.001.
+
+;; changing the implementation of good-enough? does indeed solve both
+;; of these issues (though we are still bound by floating-point
+;; precision)
+
+(define (good-enough? guess x)
+  (< (/ (abs (- guess (improve guess x)))
+	guess) 0.001))
+
+(sicp/sqrt 0.000000001)
