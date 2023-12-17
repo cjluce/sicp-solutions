1461394cbc479f6ce2af38f4076a359b09d11d9d
Cooper Luce <16cluce@gmail.com>
2023-12-17 15:06:59 -0500

    new solutions

--- chapter-2/section-1/ex-2.08.scm
+++ chapter-2/section-1/ex-2.08.scm
@@ -4,4 +4,12 @@
 
 (load "/home/lucec/classes/sicp/chapter-2/section-1/interval.scm")
 
-(define (sub-interval x y))
+;; We need to deduce what the minimum and maximum bounds would be as a
+;; result of subtracting two intervals
+;;   I         J
+;; [a, b] - [c, d]
+;; Take i in I, j in J.
+;; i - j is in [a - d, b - c]
+(define (sub-interval x y)
+  (make-interval (- (lower-bound x) (upper-bound y))
+		 (- (upper-bound x) (lower-bound y))))
