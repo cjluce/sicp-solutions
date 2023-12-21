824f26cb4c76c61927334fc17477e183ace7e4df
Cooper Luce <16cluce@gmail.com>
2023-12-20 23:54:06 -0500

    I am now using racket for the SICP compat. language

---
+++ chapter-2/section-2/ex-2.17.rkt
@@ -0,0 +1,14 @@
+#lang sicp
+
+;; *Exercise 2.17:* Define a procedure `last-pair' that returns the
+;; list that contains only the last element of a given (nonempty)
+;; list:
+
+(define (last-pair lst)
+  (if (null? (cdr lst))
+      lst
+      (last-pair (cdr lst))))
+
+(last-pair (list 23 72 149 34))
+;; (34)
+
